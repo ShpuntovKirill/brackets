9cea7702c3271fcda05079b9e020ee971a64a43f
Shpuntov Kirill <ks090192@gmail.com>
2020-10-18 02:53:12 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let bracketsArr = bracketsConfig.map( (e) => e.join("") );  //--multidimensialArr bracketsConfig to arr of pair brackets
+  for (let i=0; i < bracketsArr.length; i++) {                //--iterating arr
+    if (str.includes(bracketsArr[i])) {                       //--find pair brackets
+      str=str.replace(bracketsArr[i],"");                     //--remove pair brackets from str
+      i =-1;                                                  //--reiterating transformed arr
+    }
+  }
+  return str.length === 0;
+};
